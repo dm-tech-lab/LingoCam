7e0fb3acd1d0381a766adb41cfbf45091bbec400
Dimitar Kyuchukov <dkyuchukov2003@gmail.com>
2023-10-09 22:27:10 +0300

    switching cameras

--- client/src/screens/Camera.tsx
+++ client/src/screens/Camera.tsx
@@ -1,24 +1,30 @@
 import Webcam from "react-webcam";
 import { useWindowSize } from "../context/WindowSize";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useProtectedRoute } from "../utils/ProtectedRoutes";
+
+const FACING_MODE_USER = "user";
+const FACING_MODE_ENVIRONMENT = "environment";
+
+const videoConstraints = {
+  facingMode: FACING_MODE_USER,
+};
 
 const Camera = () => {
   useProtectedRoute();
 
   const { width, height } = useWindowSize();
   const isLandscape = width <= height;
-  const [facingMode, setFacingMode] = useState<object | string>("user");
 
-  useEffect(() => {
-    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+  const [facingMode, setFacingMode] = useState(FACING_MODE_USER);
 
-    if (isMobile) {
-      setFacingMode({ exact: "environment" }); // Use rear camera on mobile
-    } else {
-      setFacingMode("user"); // Use front camera on desktop
-    }
-  }, [width, height]);
+  const handleClick = useCallback(() => {
+    setFacingMode((prevState) =>
+      prevState === FACING_MODE_USER
+        ? FACING_MODE_ENVIRONMENT
+        : FACING_MODE_USER
+    );
+  }, []);
 
   const videoConstraints = {
     width: isLandscape ? width : height,
@@ -30,14 +36,18 @@ const Camera = () => {
     <div className="flex flex-col h-screen">
       <Webcam
         audio={false}
-        height={isLandscape ? height : width}
         screenshotFormat="image/jpeg"
-        width={isLandscape ? width : height}
-        videoConstraints={videoConstraints}
+        videoConstraints={{
+          ...videoConstraints,
+          facingMode,
+        }}
         className="flex-1"
       />
-      <button className="bg-blue-500 text-white py-2 px-4 absolute bottom-4 left-1/2 transform -translate-x-1/2 z-10">
-        Your Button
+      <button
+        onClick={handleClick}
+        className="bg-blue-500 text-white py-2 px-4 absolute bottom-4 left-1/2 transform -translate-x-1/2 z-10"
+      >
+        Switch camera
       </button>
     </div>
   );
