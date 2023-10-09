13b6855d22221c9ff62f701fc4ce1b1ede3f47d5
Martin Manchev <mmanchev23@gmail.com>
2023-10-09 22:54:15 +0300

    [Fix] OCR model architecture ...

--- server/ocr/views.py
+++ server/ocr/views.py
@@ -1,33 +1,37 @@
-import pytesseract
+import easyocr
 import numpy as np
+
 from .models import *
+
+from .serializers import ImageUploadSerializer
+
 from PIL import Image
+
+from deep_translator import GoogleTranslator
 
 from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from deep_translator import GoogleTranslator
-from .serializers import ImageUploadSerializer
+
 
 class TranslationView(APIView):
     
     def post(self, request):
         if not request.user.is_authenticated:
             return Response({ "message": "You are not logged in!" }, status=status.HTTP_401_UNAUTHORIZED)
+        
         serializer = ImageUploadSerializer(data=request.data)
         
         if serializer.is_valid():
-            pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
             image = np.array(Image.open(serializer.validated_data["image"]))
-            text = pytesseract.image_to_string(image)
-            result = GoogleTranslator(source="auto", target="bg").translate(text=str(text).strip())
-            Translation.objects.create(
-                user=request.user,
-                text=text,
-                result=result
-            )
+            reader = easyocr.Reader(["en", "de"], gpu=False)
+            extraction = str().join(reader.readtext(image, detail=0))
+            translation = GoogleTranslator(source="auto", target="bg").translate(text=str(extraction).strip())
+            Translation.objects.create(user=request.user, text=extraction, result=translation)
+            
             return Response({
-                "result": result
-            })
+                "extraction": extraction,
+                "translation": translation
+            }, status=status.HTTP_200_OK)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
