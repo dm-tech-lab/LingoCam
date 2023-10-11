6ae7ae8913db3ab38a67b3a3812ff5ce67b4dd94
Dimitar Kyuchukov <dkyuchukov2003@gmail.com>
2023-10-11 20:14:05 +0300

    fix

--- client/src/components/shared/Loader.tsx
+++ client/src/components/shared/Loader.tsx
@@ -6,7 +6,7 @@ const Loader = () => {
   return (
     <>
       {loading && (
-        <div className="fixed inset-0 flex items-center justify-center text-center bg-white z-50">
+        <div className="fixed inset-0 flex items-center justify-center text-center bg-white" style={{zIndex: "99999999"}}>
           <div role="status">
             <svg
               aria-hidden="true"
